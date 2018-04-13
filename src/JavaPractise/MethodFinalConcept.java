392f387d8ac48885adb134fc7e9edd294e1ce616
vverma8345 <vverma8345@gmail.com>
2018-04-13 09:28:40 +0530

    java activity

---
+++ src/JavaPractise/MethodFinalConcept.java
@@ -0,0 +1,13 @@
+package JavaPractise;
+
+public final class MethodFinalConcept {
+	
+	public final String method(String str)
+	{
+		
+		System.out.println("final method called"+str);
+		return str;
+		
+	}
+
+}
---
+++ src/JavaPractise/SubClass.java
@@ -0,0 +1,14 @@
+package JavaPractise;
+
+public class SubClass extends MethodFinalConcept{
+
+	public static void main(String[] args) {
+	
+		SubClass sb = new SubClass();
+		sb.method("vimal");
+		
+		MethodFinalConcept md = new MethodFinalConcept();
+		md.method("vimal");
+	}
+
+}
